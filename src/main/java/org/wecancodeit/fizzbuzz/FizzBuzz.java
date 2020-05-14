ba5fbbf1adb3ea174989bc971417f9c7deb051cf
unknown <SGDaniels08@gmail.com>
2020-05-14 15:17:56 -0400

    FizzBuzz TDD Practice Begun

--- src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
@@ -2,4 +2,14 @@ package org.wecancodeit.fizzbuzz;
 
 
 public class FizzBuzz {
+
+    public String say(int numberToTranslate) {
+        if (numberToTranslate == 3) {
+            return "Fizz";
+        }
+        if (numberToTranslate == 5) {
+            return "Buzz";
+        }
+        return "" + numberToTranslate;
+    }
 }
--- src/test/java/org/wecancodeit/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/wecancodeit/fizzbuzz/FizzBuzzTest.java
@@ -2,9 +2,75 @@ package org.wecancodeit.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
     @Test
     public void shouldInstantiate() {
         FizzBuzz underTest = new FizzBuzz();
+    }
+
+    // 1 prints "1"
+    @Test
+    public void input1PrintsString1() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(1);
+        // assertion
+        assertEquals("1", result);
+    }
+
+    // 2 prints "2"
+    @Test
+    public void input2PrintsString2() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(2);
+        // assertion
+        assertEquals("2", result);
+
+    }
+
+    // 3 prints "Fizz"
+    @Test
+    public void input3PrintsFizz() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(3);
+        // assertion
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void input4PrintsString4() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(4);
+        // assertion
+        assertEquals("4", result);
+    }
+
+    @Test
+    public void input5PrintsBuzz() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(5);
+        // assertion
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void input6PrintsFizz() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(6);
+        // assertion
+        assertEquals("Fizz", result);
     }
 }
