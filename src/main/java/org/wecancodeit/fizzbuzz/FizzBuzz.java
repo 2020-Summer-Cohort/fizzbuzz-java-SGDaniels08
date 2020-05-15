ece115c9730626ea5ab766e55b30bc05cc5c378f
Shawn Daniels <SGDaniels08@gmail.com>
2020-05-14 23:00:42 -0400

    TDD for Various Test Conditions

--- src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
@@ -1,15 +1,45 @@
 package org.wecancodeit.fizzbuzz;
 
 
+import java.util.Scanner;
+
 public class FizzBuzz {
 
+    public static void main(String[] args) {
+        FizzBuzz testObject = new FizzBuzz();
+
+        System.out.println(testObject.sayWordsInRange(1,10));
+    }
+
     public String say(int numberToTranslate) {
-        if (numberToTranslate == 3) {
+
+        if (numberToTranslate<0) {
+            return "Please enter a positive number";
+        }
+        if (numberToTranslate%3==0 && numberToTranslate%5==0) {
+            return "FizzBuzz";
+        }
+        if (numberToTranslate%3 == 0) {
             return "Fizz";
         }
-        if (numberToTranslate == 5) {
+        if (numberToTranslate%5 == 0) {
             return "Buzz";
         }
+        if (numberToTranslate%11 == 0) {
+            return "Bang";
+        }
+
         return "" + numberToTranslate;
+    }
+
+    public String sayWordsInRange(int lowerBound, int upperBound) {
+        String wordList = "";
+        for (int i = lowerBound; i <= upperBound; i++) {
+            if (say(i).equals("Fizz") || say(i).equals("Buzz") || say(i).equals("FizzBuzz")) {
+                wordList += say(i);
+            }
+        }
+        System.out.println(wordList);
+        return wordList;
     }
 }
--- src/test/java/org/wecancodeit/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/wecancodeit/fizzbuzz/FizzBuzzTest.java
@@ -73,4 +73,143 @@ public class FizzBuzzTest {
         // assertion
         assertEquals("Fizz", result);
     }
+
+    @Test
+    public void input7Prints7() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(7);
+        // assertion
+        assertEquals("7", result);
+    }
+
+    @Test
+    public void input8Prints8() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(8);
+        // assertion
+        assertEquals("8", result);
+    }
+
+    @Test
+    public void input9PrintsFizz() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(9);
+        // assertion
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void input10PrintsBuzz() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(10);
+        // assertion
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void input11PrintsBang() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(11);
+        // assertion
+        assertEquals("Bang", result);
+    }
+
+    @Test
+    public void input12PrintsFizz() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(12);
+        // assertion
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void input15PrintsFizzBuzz() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(15);
+        // assertion
+        assertEquals("FizzBuzz", result);
+    }
+
+    @Test
+    public void input18PrintsFizz() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(18);
+        // assertion
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void input20PrintsBuzz() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(20);
+        // assertion
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void input21PrintsFizz() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(21);
+        // assertion
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void input90PrintsFizzBuzz() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String result = underTest.say(90);
+        // assertion
+        assertEquals("FizzBuzz", result);
+    }
+    @Test
+    public void check1Through10GivesFizzBuzzFizzFizzBuzz() {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String resultList = underTest.sayWordsInRange(1, 10);
+        // assertion
+        assertEquals("FizzBuzzFizzFizzBuzz", resultList);
+    }
+
+    @Test
+    public void check15Through30GivesFizzBuzzFizzBuzzFizzFizzBuzzFizzFizzBuzz () {
+        // arrangement
+        FizzBuzz underTest = new FizzBuzz();
+        // action
+        String resultList = underTest.sayWordsInRange(15, 30);
+        // assertion
+        assertEquals("FizzBuzzFizzBuzzFizzFizzBuzzFizzFizzBuzz", resultList);
+    }
+
+    @Test
+    public void inputNeg1GivesMessageNeedPositive() {
+        // arrangement
+        FizzBuzz testObject = new FizzBuzz();
+        // action
+        String result = testObject.say(-1);
+        // assertion
+        assertEquals("Please enter a positive number", result);
+    }
 }
